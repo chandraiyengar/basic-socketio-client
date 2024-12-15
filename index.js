13363c29c2e624759bb90964483f06b7de6cd1af
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2024-12-15 18:36:32 +0000

    rest of code

--- index.js
+++ index.js
@@ -19,12 +19,23 @@ class EventEmitter {
   }
 }
 
+const SIOPacketType = {
+  CONNECT: 0,
+  DISCONNECT: 1,
+  EVENT:  2,
+};
+
 class Socket extends EventEmitter {
   #uri;
   #opts;
   #ws;
   #pingTimeoutTimer;
   #pingTimeoutDelay;
+  #reconnectTimer;
+  #shouldReconnect = true;
+  #sendBuffer = [];
+  id;
+  connected = false;
 
   constructor(uri, opts) {
     super();
@@ -62,10 +73,47 @@ class Socket extends EventEmitter {
         this.#send(EIOPacketType.PONG);
         break;
 
+      case EIOPacketType.MESSAGE:
+        let packet;
+        try {
+          packet = decode(data);
+        } catch(e) {
+          return this.#onClose("parse error");
+        }
+        this.#onPacket(packet);
+        break;
+
       default:
         this.#onClose("parse error");
         break;
     }
+  }
+
+  #onPacket(packet) {
+    switch (packet.type) {
+      case SIOPacketType.CONNECT:
+        this.#onConnect(packet);
+        break;
+
+      case SIOPacketType.DISCONNECT:
+        this.#shouldReconnect = false;
+        this.#onClose("io server disconnect");
+        break;
+
+      case SIOPacketType.EVENT:
+        super.emit.apply(this, packet.data);
+        break;
+    }
+  }
+
+  #onConnect(packet) {
+    this.id = packet.data.sid;
+    this.connected = true;
+
+    this.#sendBuffer.forEach((packet) => this.#sendPacket(packet));
+    this.#sendBuffer.slice(0);
+
+    super.emit("connect");
   }
 
   #onOpen(data) {
@@ -77,6 +125,15 @@ class Socket extends EventEmitter {
     }
     this.#pingTimeoutDelay = handshake.pingInterval + handshake.pingTimeout;
     this.#resetPingTimeout();
+    this.#doConnect();
+  }
+
+  #doConnect() {
+    this.#sendPacket({ type: SIOPacketType.CONNECT });
+  }
+  
+  #sendPacket(packet) {
+    this.#send(EIOPacketType.MESSAGE + encode(packet));
   }
 
   #resetPingTimeout() {
@@ -100,14 +157,81 @@ class Socket extends EventEmitter {
     }
 
     clearTimeout(this.#pingTimeoutTimer);
+    clearTimeout(this.#reconnectTimer);
 
-    setTimeout(() => this.#open(), this.#opts.reconnectionDelay);
+    if (this.#shouldReconnect) {
+      this.#reconnectTimer = setTimeout(
+        () => this.#open(),
+        this.#opts.reconnectionDelay
+      );
+    }
+  }
+
+  disconnect() {
+    this.#shouldReconnect = false;
+    this.#onClose("io client disconnect");
   }
 
   #createUrl() {
     const uri = this.#uri.replace(/^http/, "ws");
     const queryParams = "?EIO=4&transport=websocket";
     return `${uri}${this.#opts.path}${queryParams}`;
+  }
+
+  emit(...args) {
+    const packet = {
+      type: SIOPacketType.EVENT,
+      data: args,
+    };
+
+    if (this.connected) {
+      this.#sendPacket(protocol);
+    } else {
+      this.#sendBuffer.push(packet);
+    }
+  }
+}
+function decode(data) {
+  let i = 1;
+
+  const packet = {
+    type: parseInt(data.charAt(i++), 10),
+  };
+
+  if (data.charAt(i)) {
+    packet.data = JSON.parse(data.substring(i));
+  }
+
+  if (!isPacketValid(packet)) {
+    throw new Error("invalid format");
+  }
+
+  return packet;
+}
+
+function encode(packet) {
+  let output = "" + packet.type;
+
+  if (packet.data) {
+    output += JSON.stringify(packet.data);
+  }
+
+  return output;
+}
+
+function isPacketValid(packet) {
+  switch (packet.type) {
+    case SIOPacketType.CONNECT:
+      return typeof packet.data === "object";
+    case SIOPacketType.DISCONNECT:
+      return packet.data === undefined;
+    case SIOPacketType.EVENT:
+      const args = packet.data;
+      return (
+        Array.isArray(args) && args.length > 0 && typeof args[0] === "string"
+      );
+    default:
+      return false;
   }
 }
 
